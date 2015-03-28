b9ab1158660d9177456c9d04d4f284a615adf231
James <mail@employ.com>
2015-03-28 19:42:22 -0400

    Update main.js

--- new3/js/main.js
+++ new3/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $(".searchbtn").click(function() {
-        $("#resultsnav").slideToggle( "50", "linear" );
+        $("#resultsnav").slideToggle( "20", "linear" );
     });
 });
