705ef9bbe2ab39aef7c3664fe11b43fdedfe5f4d
James <mail@employ.com>
2015-04-25 21:16:07 +0100

    Update main.js

--- new3/js/main.js
+++ new3/js/main.js
@@ -4,3 +4,21 @@ $(document).ready(function() {
         $("#resultsnav").toggle();
     });
 });
+
+$(document).ready(function() {
+    $('.click-nav > ul').toggleClass('no-js js');
+    $('.click-nav .js ul').hide();
+    $('.click-nav .js li').click(function (e) {
+        $('.click-nav .js li ul').hide();
+        $('.clicker').removeClass('active')
+        $('ul',this).toggle();
+        $('.clicker',this).toggleClass('active');
+        e.stopPropagation();
+    });
+    $(document).click(function () {
+        if ($('.click-nav .js ul').is(':visible')) {
+            $('.click-nav .js ul', this).slideUp();
+            $('.clicker').removeClass('active');
+        }
+    });
+});
