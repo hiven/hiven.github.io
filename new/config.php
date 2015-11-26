1af2aeaaa48f4d2dfcdafc4e4b47e697bf7ef920
hiven <james@hiven.co.uk>
2015-11-26 12:46:14 +0000

    Update config.php

--- new/config.php
+++ new/config.php
@@ -2,15 +2,13 @@
     //set off all error for security purposes
 	error_reporting(E_ALL);
 	
-
-	//define some contstant
+    //define some contstant
     define( "DB_DSN", "mysql:host=localhost;dbname=codecalltut" );
     define( "DB_USERNAME", "root" );
     define( "DB_PASSWORD", "" );
-	define( "CLS_PATH", "class" );
+    define( "CLS_PATH", "class" );
+    
+    //include the classes
+    include_once( CLS_PATH . "/user.php" );
 	
-	//include the classes
-	include_once( CLS_PATH . "/user.php" );
-	
-
 ?>
