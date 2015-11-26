eaf5867d8bb7cc3d77f2ca34a37b3cd61c91a5fe
hiven <james@hiven.co.uk>
2015-11-26 11:34:22 +0000

    Delete main.sql

--- new/sql/main.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `active` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-)
