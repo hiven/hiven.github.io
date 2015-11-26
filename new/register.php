a5dcd8101367e3ac41a80a3ae05a2c8a2b513ede
hiven <james@hiven.co.uk>
2015-11-26 14:50:29 +0000

    Update register.php

--- new/register.php
+++ new/register.php
@@ -1,24 +1,28 @@
-<?php 
-    include_once("config.php");
-?>
-
-<?php if( !(isset( $_POST['register'] ) ) ) { ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<div class="header">
+    <ul>
+        <li class="active"><a href="index.html">Home</a></li>
+        <li><a href="features.html">Features</a></li>
+        <li><a href="register.html">Register</a></li>
+        <li><a href="login.html">Login</a></li>
+    </ul>
+</div>
+<div class="shout">
     <form method="post">
         <input type="text" id="usn" maxlength="30" required autofocus name="username" />
         <input type="password" id="passwd" maxlength="30" required name="password" />
         <input type="password" id="conpasswd" maxlength="30" required name="conpassword" />
         <input type="submit" name="register" value="Register" />
     </form>
-
-<?php 
-} else {
-    $usr = new Users;
-    $usr->storeFormValues( $_POST );
-	
-    if( $_POST['password'] == $_POST['conpassword'] ) {
-        echo $usr->register($_POST);	
-    } else {
-        echo "Password and Confirm password not match";	
-    }
-}
-?>
+</div>
+<div class="footer">
+    <ul>
+        <li><a href="#">Terms</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+</div>
