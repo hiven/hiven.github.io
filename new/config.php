176cc8d48cc208288e4ef699f986ab640a26932d
hiven <james@hiven.co.uk>
2015-11-26 12:46:26 +0000

    Update config.php

--- new/config.php
+++ new/config.php
@@ -1,6 +1,6 @@
 <?php
     //set off all error for security purposes
-	error_reporting(E_ALL);
+    error_reporting(E_ALL);
 	
     //define some contstant
     define( "DB_DSN", "mysql:host=localhost;dbname=codecalltut" );
