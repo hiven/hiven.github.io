e787e8f8800d4c893eb88d1af3411c4dce548a9c
hiven <james@hiven.co.uk>
2015-11-26 14:52:14 +0000

    Update login.php

--- new/login.php
+++ new/login.php
@@ -1,21 +1,27 @@
-<?php 
-    include_once("config.php");
-?>
-<?php if( !(isset( $_POST['login'] ) ) ) { ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<div class="header">
+    <ul>
+        <li class="active"><a href="index.html">Home</a></li>
+        <li><a href="features.html">Features</a></li>
+        <li><a href="register.html">Register</a></li>
+        <li><a href="login.html">Login</a></li>
+    </ul>
+</div>
+<div>
     <form method="post" action="">
         <input type="text" maxlength="30" required autofocus name="username" />
         <input type="password" maxlength="30" required name="password" />
         <input type="submit" name="login" value="Log me in" />
     </form>
-<?php 
-} else {
-    $usr = new Users;
-    $usr->storeFormValues( $_POST );
-	
-    if( $usr->userLogin() ) {
-        echo "Welcome";	
-    } else {
-        echo "Incorrect Username/Password";	
-    }
-}
-?>
+</div>
+<div class="footer">
+    <ul>
+        <li><a href="#">Terms</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+</div>
