7ab543cbe57919cc60edc4c3e3ae5a6be57ce226
hiven <james@hiven.co.uk>
2015-11-27 10:51:24 +0000

    Delete contact.php

--- new/contact.php
+++
@@ -1,7 +0,0 @@
-<form class="email" action="mailer.php" method="post">
-    <input type="text" name="name" />
-    <input type="text" name="email" />
-    <input type="text" name="subject" />
-    <textarea name="message"></textarea></p>
-    <input class="send" type="submit" value="Send">
-</form>
