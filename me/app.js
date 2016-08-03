d9905628ad7fe7030b6ffd179113d382491dcc81
hiven <james@hiven.co.uk>
2016-08-03 10:28:55 +0100

    Update app.js

--- me/app.js
+++ me/app.js
@@ -4,21 +4,21 @@ function startScript()
 {
 	$(document).bind("ready", function(){
 		$("#encryptBtn").click(function(){
-			var data = $("#dataTxt").val();
+			var data = $("#inputTxt").val();
 			var key = $("#secretTxt").val();
 			var encrypted = CryptoJS.AES.encrypt(data,key);
 			console.log(encrypted);
-			$("#dataTxt").val(encrypted);
+			$("#ouputTxt").val(encrypted);
 		});
 		
 		$("#decryptBtn").click(function(){
-			var encrypted = $("#dataTxt").val();
+			var encrypted = $("#inputTxt").val();
 			var key = $("#secretTxt").val();
 			try
 			{
 				var data = CryptoJS.AES.decrypt(encrypted,key);
 				console.log(data);
-				$("#dataTxt").val(data.toString(CryptoJS.enc.Utf8));
+				$("#outputTxt").val(data.toString(CryptoJS.enc.Utf8));
 			} 
 			catch (e)
 			{
