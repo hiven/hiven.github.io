8d8a680ad9459ff1c2c628ec0747bec7fc0d15fd
hiven <james@hiven.co.uk>
2017-12-25 18:21:52 +0000

    Update script.js

--- wedding/script.js
+++ wedding/script.js
@@ -1,1 +1,19 @@
-test
+function show() {
+  /* Cache the collection. */
+  var extra = document.getElementsByClassName('extra');
+
+  /* Iterate over every element in the collection. */
+  [].forEach.call(extra, function (element) {
+     element.style.display = 'block';
+  });
+}
+
+function hide() {
+  /* Cache the collection. */
+  var extra = document.getElementsByClassName('extra');
+
+  /* Iterate over every element in the collection. */
+  [].forEach.call(extra, function (element) {
+     element.style.display = 'none';
+  });
+}
