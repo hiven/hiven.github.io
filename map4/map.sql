3d40c51b2b371fc463fda4522785490a95d6a7b3
hiven <mail@hiven.com>
2021-12-28 12:17:04 -0500

    Update map.sql

--- map4/map.sql
+++ map4/map.sql
@@ -8,5 +8,8 @@ CREATE TABLE `Grid` (
   PRIMARY KEY (`id`)
 ) ENGINE=INNODB; CHARACTER SET utf8
 
-INSERT INTO `Grid`(point, polygon) 
-VALUES(ST_PointFromText('POINT(49.227239 17.564932)'),ST_PolygonFromText('POLYGON((0 5, 2 5, 2 7, 0 7, 0 5))'));
+//INSERT INTO `Grid`(point, polygon) 
+//VALUES(ST_PointFromText('POINT(49.227239 17.564932)'),ST_PolygonFromText('POLYGON((0 5, 2 5, 2 7, 0 7, 0 5))'));
+
+INSERT INTO Grid (point)
+VALUES(Point(1,2));
