551c0f91d346877238b8705848375880dd36aa8c
hiven <mail@hiven.com>
2021-12-28 09:34:09 -0500

    Create map.sql

---
+++ map4/map.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `Grid` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `point` POINT DEFAULT NULL,
+  `polygon` POLYGON DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB; CHARACTER SET utf8
+
+INSERT INTO `Grid`(point, polygon) 
+VALUES(ST_PointFromText('POINT(49.227239 17.564932)'),ST_PolygonFromText('POLYGON((0 5, 2 5, 2 7, 0 7, 0 5))'));
